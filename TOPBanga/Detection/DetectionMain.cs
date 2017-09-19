d2b49e34ef6f7be736002b32bef97d74b88176e6
Paulius Kalvaitis <pauliuskalv@gmail.com>
2017-09-19 23:13:35 +0300

    Initialize the Detection namespace. Create the DetectionMain class and move code from MainForm to it

---
+++ TOPBanga/Detection/DetectionMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.OCR;
+using Emgu.CV.Structure;
+using System.Linq;
+
+namespace Detection
+{
+    class DetectionMain
+    {
+        private double min_dist = 40.0;
+        private int min_radius = 8;
+        private int max_radius = 150;
+        private double resolution = 0.0001f;
+        private const int canny = 60;
+        private const int accumulator = 28;
+        public DetectionMain()
+        {
+
+        }
+        public Image<Bgr,Byte> CalcImage( Image<Bgr,Byte> img )
+        {
+            CvInvoke.MedianBlur(img, img, 3);
+            Image<Gray, Byte> gray = img.Convert<Gray, Byte>();
+            Gray cannyThreshold = new Gray(canny);
+            Gray cannyThresholdLinking = new Gray(120);
+            Gray circleAccumulatorThreshold = new Gray(accumulator);
+            CircleF[] circles = gray.HoughCircles(
+                cannyThreshold,
+                circleAccumulatorThreshold,
+                resolution,
+                min_dist,
+                min_radius,
+                max_radius
+                )[0];
+            for (int i = 0; i != circles.Count(); i++)
+                img.Draw(circles[i], new Bgr(), 1, LineType.EightConnected, 0);
+
+            return img;
+        }
+    }
+}
--- TOPBanga/MainForm.cs
+++ TOPBanga/MainForm.cs
@@ -11,23 +11,13 @@ namespace Test
 {
     public partial class MainForm : Form
     {
-        private double min_dist = 40.0;
-        private int min_radius = 8;
-        private int max_radius = 150;
-        private double resolution = 0.0001f;
         private IImage image;
         private Tesseract t;
         private VideoCapture video;
         private System.Threading.Timer timer;
-        private const int canny = 60;
-        private const int accumulator = 28;
         public MainForm()
         {
             InitializeComponent();
-            this.Resolution.Text = "" + resolution.ToString("0.00000") ;
-            this.MinDistValue.Text = "" + min_dist;
-            this.MinRadValue.Text = "" + min_radius;
-            this.MaxRadValue.Text = "" + max_radius;
             t = new Tesseract("", "eng", OcrEngineMode.Default);
         }
         private void MainForm_Load(object sender, EventArgs e)
@@ -35,31 +25,6 @@ namespace Test
         }
         private void MainForm_Resize(object sender, EventArgs e)
         {
-        }
-        private void calcImage()
-        {
-            Image<Bgr, Byte> img = new Image<Bgr, byte>(image.Bitmap);
-            /*
-             * This part, surprisingly, helps!
-             */
-            CvInvoke.MedianBlur(img, img, 3);
-            Image<Gray, Byte> gray = img.Convert<Gray, Byte>();
-            Gray cannyThreshold = new Gray(canny);
-            Gray cannyThresholdLinking = new Gray(120);
-            Gray circleAccumulatorThreshold = new Gray(accumulator);
-            CircleF[] circles = gray.HoughCircles(
-                cannyThreshold,
-                circleAccumulatorThreshold,
-                resolution, //Resolution of the accumulator used to detect centers of the circles
-                min_dist, //min distance 
-                min_radius, //min radius
-                max_radius //max radius
-                )[0]; //Get the circles from the first channel
-            for (int i = 0; i != circles.Count(); i++)
-                img.Draw(circles[i], new Bgr(), 1, LineType.EightConnected, 0);
-
-            Picture.Image = img.ToBitmap();
-            System.GC.Collect();
         }
 
         private void calcFrame(Object stateInfo) {
@@ -101,7 +66,7 @@ namespace Test
                 if ( extension.Contains("png") || extension.Contains("jpg") )
                 {
                     image = new Image<Bgr, Byte>(Openfile.FileName);
-                    calcImage();
+                    
                     return;
                 }
 
@@ -112,7 +77,7 @@ namespace Test
 
         private void Refresh_Button_Click(object sender, EventArgs e)
         {
-            if (image != null) calcImage();
+            
         }
 
         private void Picture_Click(object sender, EventArgs e)
@@ -122,26 +87,22 @@ namespace Test
 
         private void minDist_Click(object sender, EventArgs e)
         {
-            min_dist = int.Parse(MinDistValue.Text);
-            if (image != null) calcImage();
+            
         }
 
         private void changeResolution_Click(object sender, EventArgs e)
         {
-            resolution = Double.Parse(Resolution.Text);
-            if (image != null) calcImage();
+            
         }
 
         private void changeMinRadius_Click(object sender, EventArgs e)
         {
-            min_radius = int.Parse(MinRadValue.Text);
-            if (image != null) calcImage();
+           
         }
 
         private void changeMaxRadius_Click(object sender, EventArgs e)
         {
-            max_radius = int.Parse(MaxRadValue.Text);
-            if (image != null) calcImage();
+            
         }
 
     }
