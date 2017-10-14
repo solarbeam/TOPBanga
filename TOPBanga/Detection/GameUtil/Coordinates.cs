690a3d72af1363e020b00730675c5c9a012c4f9d
Paulius Kalvaitis <pauliuskalv@gmail.com>
2017-10-14 17:56:16 +0300

    Initial branch commit. Implement checking by utilizing linear interpolation.

---
+++ TOPBanga/Detection/GameUtil/Coordinates.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOPBanga.Detection.GameUtil
+{
+    /**
+     * For now it's a class, but we really should
+     * convert this class to a struct
+     */
+    class Coordinates
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public Coordinates(double X, double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+}
---
+++ TOPBanga/Detection/GameUtil/GoalChecker.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOPBanga.Detection.GameUtil
+{
+    class GoalChecker
+    {
+        /**
+         * Used to define the goal checking accuracy
+         */
+        const uint iterations = 10;
+        const double toAdd = 1 / iterations;
+        static Boolean Check(GoalZone zone,Coordinates ballPos)
+        {
+            double allowedDiff;
+            Coordinates toCheck;
+            /**
+             * Start by checking the bottom edge
+             */
+            allowedDiff = (zone.bottomLeft.X + zone.bottomRight.X) / iterations;
+            for (double i = 0; i < 1; i += toAdd)
+            {
+                toCheck = getHalfwayPoint(zone.bottomLeft, zone.bottomRight, i);
+                if (getDiff(ballPos.X, toCheck.X) <= allowedDiff)
+                {
+                    if (ballPos.Y > toCheck.Y)
+                    {
+                        break;
+                    }
+                    else
+                        return false;
+                }
+                else
+                    continue;
+            }
+            /**
+             * Check left edge
+             */
+            allowedDiff = (zone.bottomLeft.Y + zone.topLeft.Y) / iterations;
+            for (double i = 0; i < 1; i += toAdd)
+            {
+                toCheck = getHalfwayPoint(zone.bottomLeft, zone.topLeft, i);
+                if (getDiff(ballPos.Y, toCheck.Y) <= allowedDiff)
+                {
+                    if (ballPos.X > toCheck.X)
+                    {
+                        break;
+                    }
+                    else
+                        return false;
+                }
+                else
+                    continue;
+            }
+            /**
+             * Check top edge
+             */
+            allowedDiff = (zone.topLeft.X + zone.topRight.X) / iterations;
+            for (double i = 0; i < 1; i += toAdd)
+            {
+                toCheck = getHalfwayPoint(zone.topLeft, zone.topRight, i);
+                if (getDiff(ballPos.X, toCheck.X) <= allowedDiff)
+                {
+                    if (ballPos.Y < toCheck.Y)
+                    {
+                        break;
+                    }
+                    else
+                        return false;
+                }
+                else
+                    continue;
+            }
+            /**
+             * Finally, check the left edge
+             */
+            allowedDiff = (zone.topRight.Y + zone.bottomRight.Y) / iterations;
+            for (double i = 0; i < 1; i += toAdd)
+            {
+                toCheck = getHalfwayPoint(zone.topRight, zone.bottomRight, i);
+                if (getDiff(ballPos.Y, toCheck.Y) <= allowedDiff)
+                {
+                    if (ballPos.X > toCheck.X)
+                    {
+                        return true;
+                    }
+                    else
+                        return false;
+                }
+                else
+                    continue;
+            }
+            return false;
+        }
+        private static Coordinates getHalfwayPoint(Coordinates one, Coordinates two, double coefficient)
+        {
+            double x, y;
+
+            x = (1 - coefficient) * one.X + coefficient * two.X;
+            y = (1 - coefficient) * one.Y + coefficient * two.Y;
+
+            return new Coordinates(x, y);
+        }
+        private static double getDiff(double one, double two)
+        {
+            if (one > two)
+            {
+                return one - two;
+            }
+            else
+                return two - one;
+        }
+    }
+}
