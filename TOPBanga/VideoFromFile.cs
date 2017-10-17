23283650898207182ae2fb802a76507e45b358d9
Zilvinas <bartkuszilvinas@gmail.com>
2017-10-17 16:00:39 +0300

    Fixing designer file issues

--- TOPBanga/VideoFromFile.cs
+++ TOPBanga/VideoFromFile.cs
@@ -93,6 +93,7 @@ namespace TOPBanga
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //a
             this.videoTickTimer.Interval = 80;
             if (this.videoLoaded)
             {
