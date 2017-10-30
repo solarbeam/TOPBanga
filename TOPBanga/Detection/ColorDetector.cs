dd3d560693b9ef4380299af41a481dbb9eff7125
Paulius Kalvaitis <pauliuskalv@gmail.com>
2017-10-30 21:48:04 +0200

    Remove unneeded code. Assign x and y values from the blob's centroid.

--- TOPBanga/Detection/ColorDetector.cs
+++ TOPBanga/Detection/ColorDetector.cs
@@ -46,67 +46,43 @@ namespace TOPBanga.Detection
             bitmap = null;
             int maxRadius = 20;
             Image<Hsv, byte> hsvImg = this.image.Convert<Hsv, byte>();
-            /*
-             * Switch this bool to enable blob detection
-             */
-            bool blobTest = true;
 
             Hsv lowerLimit = new Hsv(ballHsv.Hue - this.threshold, ballHsv.Satuation - this.threshold, ballHsv.Value - this.threshold);
             Hsv upperLimit = new Hsv(ballHsv.Hue + this.threshold, ballHsv.Satuation + this.threshold, ballHsv.Value + this.threshold);
 
             Image<Gray, byte> imgFiltered = hsvImg.InRange(lowerLimit, upperLimit);
 
-            if (blobTest == true)
+            BlobDetector detector = new BlobDetector();
+            CvBlobs points = new CvBlobs();
+            uint count;
+
+            count = detector.GetBlobs(imgFiltered, points);
+
+            if (count == 0)
             {
-                BlobDetector detector = new BlobDetector();
-                CvBlobs points = new CvBlobs();
-                uint count;
-
-                count = detector.GetBlobs(imgFiltered, points);
-
-                if (count == 0)
-                {
-                    /**
-                     * Will change this in the future
-                     */
-                    success = true;
-                    bitmap = this.image.Bitmap;
-                    return true;
-                }
-
                 /**
-                 * Sort blobs by the amount of pixels in them
+                 * Will change this in the future
                  */
-                points.OrderBy(b => b.Value.Area);
-
-                if (points.Count != 0)
-                {
-                    /**
-                     * Paint the blob with the highest area
-                     */
-                    this.image.Draw(points[1].BoundingBox, new Bgr(255,255,255), 2);
-                    success = true;
-                }
+                success = true;
+                bitmap = this.image.Bitmap;
+                return true;
             }
-            else
+
+            /**
+             * Sort blobs by the amount of pixels in them
+             */
+            points.OrderBy(b => b.Value.Area);
+
+            if (points.Count != 0)
             {
-                CircleF[] circles = imgFiltered.HoughCircles(new Gray(cannyThreshold), new Gray(accumulatorThreshold), resolution, minDist, HoughMinRadius, HoughMaxRadius)[0];
-
-                IEnumerable<CircleF> circlesFiltered =
-                    from circle in circles
-                    where circle.Radius > minRadius
-                         && circle.Radius < maxRadius
-                    select circle;
-
-                foreach (CircleF c in circlesFiltered)
-                {
-                    this.image.Draw(c, this.circleColor, circleWidth);
-                    success = true;
-                    x = c.Center.X;
-                    y = c.Center.Y;
-                    radius = c.Radius;
-                }
-            }        
+                /**
+                 * Paint the blob with the highest area
+                 */
+                this.image.Draw(points[1].BoundingBox, new Bgr(255,255,255), 2);
+                x = points[1].Centroid.X;
+                y = points[1].Centroid.Y;
+                success = true;
+            }  
 
             if (success)
             {
