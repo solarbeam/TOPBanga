f6a84ca5a4027fa3457d6da46f9ce6c9a63891b6
pauliuskalv <pauliuskalv@gmail.com>
2017-11-20 09:13:59 +0200

    Add first version of camera implementation

--- FoosLiveAndroid/FoosLiveAndroid/GameActivity.cs
+++ FoosLiveAndroid/FoosLiveAndroid/GameActivity.cs
@@ -10,15 +10,24 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Graphics;
+using Emgu.CV;
+using Camera = Android.Hardware.Camera;
+using Java.Lang;
+using Android.Media;
 
 namespace FoosLiveAndroid
 {
     [Activity(MainLauncher = true, ScreenOrientation = ScreenOrientation.Landscape)]
-    public class GameActivity : Activity
+    public class GameActivity : Activity, TextureView.ISurfaceTextureListener
     {
         private Button _gameButton;
         private TextView _score;
         private TextureView _gameView;
+        private VideoCapture videoCapture;
+        private ImageReader imageReader;
+
+        private Camera camera;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -26,8 +35,50 @@ namespace FoosLiveAndroid
             SetContentView(Resource.Layout.activity_game);
             //hides notification bar
             Window.SetFlags(WindowManagerFlags.Fullscreen, WindowManagerFlags.Fullscreen);
-
             GetReferencesFromLayout();
+
+            /**
+             * Open the camera
+             */
+            this._gameView = new TextureView(this);
+            this._gameView.SurfaceTextureListener = this;
+            //Sitas rodo kamera, uncommentinkit
+            //SetContentView(this._gameView);
+        }
+
+        public void OnSurfaceTextureAvailable(SurfaceTexture surface, int w, int h)
+        {
+            this.camera = Camera.Open();
+
+            this._gameView.LayoutParameters = new FrameLayout.LayoutParams(w, h);
+
+            try
+            {
+                this.camera.SetPreviewTexture(surface);
+                this.camera.StartPreview();
+            }
+            catch (Java.IO.IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public bool OnSurfaceTextureDestroyed(SurfaceTexture surface)
+        {
+            this.camera.StopPreview();
+            this.camera.Release();
+
+            return true;
+        }
+
+        public void OnSurfaceTextureSizeChanged(SurfaceTexture surface, int w, int h)
+        {
+
+        }
+
+        public void OnSurfaceTextureUpdated(SurfaceTexture surface)
+        {
+
         }
 
         private void GetReferencesFromLayout()
