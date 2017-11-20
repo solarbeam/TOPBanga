e27cb51299f56ff6cc7729296745e427b41b7dd6
pauliuskalv <pauliuskalv@gmail.com>
2017-11-20 19:48:31 +0200

    Add hsv selection capability

--- FoosLiveAndroid/FoosLiveAndroid/GameActivity.cs
+++ FoosLiveAndroid/FoosLiveAndroid/GameActivity.cs
@@ -15,19 +15,21 @@ using Emgu.CV;
 using Camera = Android.Hardware.Camera;
 using Java.Lang;
 using Android.Media;
+using Emgu.CV.Structure;
 
 namespace FoosLiveAndroid
 {
     [Activity(MainLauncher = true, ScreenOrientation = ScreenOrientation.Landscape)]
-    public class GameActivity : Activity, TextureView.ISurfaceTextureListener
+    public class GameActivity : Activity, TextureView.ISurfaceTextureListener, View.IOnTouchListener
     {
         private Button _gameButton;
         private TextView _score;
         private TextureView _gameView;
-        private VideoCapture videoCapture;
-        private ImageReader imageReader;
+        private SurfaceView surfaceView;
 
         private Camera camera;
+        private Hsv selectedHsv;
+        private bool hsvSelected;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -40,16 +42,15 @@ namespace FoosLiveAndroid
             /**
              * Open the camera
              */
-            this._gameView = new TextureView(this);
             this._gameView.SurfaceTextureListener = this;
-            //Sitas rodo kamera, uncommentinkit
-            //SetContentView(this._gameView);
+            this._gameView.SetOnTouchListener(this);
         }
 
         public void OnSurfaceTextureAvailable(SurfaceTexture surface, int w, int h)
         {
             this.camera = Camera.Open();
 
+            System.Console.WriteLine("Width: " + w + " Height: " + h);
             this._gameView.LayoutParameters = new FrameLayout.LayoutParams(w, h);
 
             try
@@ -78,7 +79,7 @@ namespace FoosLiveAndroid
 
         public void OnSurfaceTextureUpdated(SurfaceTexture surface)
         {
-
+            
         }
 
         private void GetReferencesFromLayout()
@@ -86,6 +87,20 @@ namespace FoosLiveAndroid
             _gameButton = FindViewById<Button>(Resource.Id.gameButton);
             _gameView = FindViewById<TextureView>(Resource.Id.game_content);
             _score = FindViewById<TextView>(Resource.Id.score);
+        }
+
+        public bool OnTouch(View v, MotionEvent e)
+        {
+            if ( !hsvSelected )
+            {
+                Image<Bgr, byte> image = new Image<Bgr, byte>(this._gameView.Bitmap);
+                this.selectedHsv = new Hsv(image.Data[(int)e.GetY(), (int)e.GetX(), 0],
+                                            image.Data[(int)e.GetY(), (int)e.GetX(), 1],
+                                            image.Data[(int)e.GetY(), (int)e.GetX(), 2]);
+                System.Console.WriteLine(this.selectedHsv.ToString());
+                this.hsvSelected = true;
+            }
+            return true;
         }
     }
 }
