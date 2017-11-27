97ae91f4288de8770779356fe9326de00e2c5d46
Zilvinas <bartkuszilvinas@gmail.com>
2017-11-27 17:05:20 +0200

    merge conflict fix

--- TOPBanga/Detection/GameUtil/GameController.cs
+++ TOPBanga/Detection/GameUtil/GameController.cs
@@ -12,20 +12,19 @@ namespace TOPBanga.Detection.GameUtil
 
     public class GameController
     {
-        
+
         public event EventHandler<EventArgs> GoalEvent;
         public int redScore { get; private set; }
         public int blueScore { get; private set; }
+        private const int SPACE_FOR_GOALS = 25;
         public EventLog EventLog { get; set; }
-        private string goalAnnouncement = "Goal!";
-        private Queue<Goal> goals = new Queue<Goal>();
         private Goal first;
         private Goal second;
+        private Queue<Goal> goals = new Queue<Goal>();
+        private string goalAnnouncement = "Goal!";
         private const int MAXIMUM_BALL_COORDINATE_NUMBER = 20;
         private const int GOAL_FRAMES_TO_COUNT_GOAL = 3;
         private PointF last_ball_coordinates;
-        
-
 
         public PointF lastBallCoordinates {
 
@@ -43,13 +42,35 @@ namespace TOPBanga.Detection.GameUtil
         }
 
         public Queue<PointF> ballCoordinates;
-        
 
+
+        public GraphicsPath table { get; private set; }
+        public void SetTable(PointF[] points)
+        {
+            if (points.Length != 4)
+                return;
+            this.table = new GraphicsPath();
+            this.table.AddPolygon(points);
+            PointF[] goalPoint = new PointF[4];
+            float xMiddle = (points[0].X + points[1].X)/2;
+            goalPoint[0] = new PointF(xMiddle - SPACE_FOR_GOALS, points[0].Y);
+            goalPoint[1] = new PointF(xMiddle + SPACE_FOR_GOALS, points[0].Y);
+            goalPoint[2] = new PointF(xMiddle + SPACE_FOR_GOALS, points[0].Y + SPACE_FOR_GOALS);
+            goalPoint[3] = new PointF(xMiddle - SPACE_FOR_GOALS, points[0].Y + SPACE_FOR_GOALS);
+            this.AddGoal(goalPoint);
+            goalPoint = new PointF[4];
+            xMiddle = (points[2].X + points[3].X) / 2;
+            goalPoint[0] = new PointF(xMiddle - SPACE_FOR_GOALS, points[2].Y);
+            goalPoint[1] = new PointF(xMiddle + SPACE_FOR_GOALS, points[2].Y);
+            goalPoint[2] = new PointF(xMiddle + SPACE_FOR_GOALS, points[2].Y - SPACE_FOR_GOALS);
+            goalPoint[3] = new PointF(xMiddle - SPACE_FOR_GOALS, points[2].Y - SPACE_FOR_GOALS);
+            this.AddGoal(goalPoint);
+        }
         public GameController()
         {
             this.ballCoordinates = new Queue<PointF>();
             this.lastBallCoordinates = new PointF(0, 0);
-            this.GoalEvent += ((obj, args) => System.Console.WriteLine("GOAL")); // will be used for sound playback
+            //this.GoalEvent += ((obj, args) => System.Console.WriteLine("GOAL")); // for preview
         }
 
         public GameController(IWrite EventLog, string filepath)
@@ -65,11 +86,12 @@ namespace TOPBanga.Detection.GameUtil
             Graphics graphics = Graphics.FromImage(bitmap);
             Pen bluePen = new Pen(Color.Blue);
             Pen redPen = new Pen(Color.Red);
+            graphics.DrawPath(bluePen, this.table);
             foreach (Goal goal in goals)
             {
                 if (first.graphicsPath.IsVisible(this.lastBallCoordinates))
                 {
-                  graphics.DrawPath(redPen, first.graphicsPath);
+                    graphics.DrawPath(redPen, first.graphicsPath);
                 }
                 else
                     graphics.DrawPath(bluePen, first.graphicsPath);
@@ -88,12 +110,10 @@ namespace TOPBanga.Detection.GameUtil
 
         public void AddGoal(PointF[] points)
         {
-            
-            
             if (this.goals.Count == 2)
             {
                 Goal toDispose = this.goals.Dequeue();
-                toDispose.Dispose();                
+                toDispose.Dispose();
             }
             GraphicsPath temp = new GraphicsPath();
             temp.AddPolygon(points);
@@ -106,44 +126,40 @@ namespace TOPBanga.Detection.GameUtil
                 second = new Goal(temp);
             }
             this.goals.Enqueue(new Goal(temp));
-            
         }
-
 
         private void OnNewFrame()
         {
-           foreach (Goal goal in goals)
-           {
+            foreach (Goal goal in goals)
+            {
+                if (first.graphicsPath.IsVisible(this.lastBallCoordinates))
+                {
+                    first.framesBallInGoal++;
+                    if (first.framesBallInGoal == GOAL_FRAMES_TO_COUNT_GOAL)
+                    {
+                        this.redScore++;
+                        GoalEvent(this, new EventArgs());
+                    }
+                }
+                else
+                {
+                    first.framesBallInGoal = 0;
+                }
                 
-                    if (first.graphicsPath.IsVisible(this.lastBallCoordinates))
+                if (second.graphicsPath.IsVisible(this.lastBallCoordinates))
+                {
+                    second.framesBallInGoal++;
+                    if (second.framesBallInGoal == GOAL_FRAMES_TO_COUNT_GOAL)
                     {
-                        first.framesBallInGoal++;
-                        if (first.framesBallInGoal == GOAL_FRAMES_TO_COUNT_GOAL)
-                        {
-                            this.redScore++;
-                            GoalEvent(this, new EventArgs());
-                        }
+                        this.blueScore++;
+                        GoalEvent(this, new EventArgs());
                     }
-                    else
-                    {
-                        first.framesBallInGoal = 0;
-                    }
+                }
+                else {
+                    goal.framesBallInGoal = 0;
 
-
-                    if (second.graphicsPath.IsVisible(this.lastBallCoordinates))
-                    {
-                        second.framesBallInGoal++;
-                        if (second.framesBallInGoal == GOAL_FRAMES_TO_COUNT_GOAL)
-                        {
-                            this.blueScore++;
-                            GoalEvent(this, new EventArgs());
-                        }
-                    }
-                    else
-                    {
-                        second.framesBallInGoal = 0;
-                    }
-           }
+                }
+            }
         }
     }
 
