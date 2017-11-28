e4142b10b6d1475a7e7fbc321b382c59d7881a8a
solarbeam <butkinas@gmail.com>
2017-11-28 16:52:56 +0200

    Add InsertIntoHistory operation database access

---
+++ FoosLiveAndroid/FoosLiveAndroid/Util/Database/DatabaseExecutor.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace FoosLiveAndroid.Util
+{
+    public static class DatabaseExecutor
+    {
+
+        public static bool InsertIntoHistory(string blueTeamName, string redTeamName, int bluePoints, int redPoints)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("http://yu-gi-oh.lt/a.php");
+            httpWebRequest.Method = "POST";
+            StreamWriter streamWriter = new StreamWriter(httpWebRequest.GetRequestStream());
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("InsertIntoHistory;");
+            stringBuilder.Append(blueTeamName);
+            stringBuilder.Append(";");
+            stringBuilder.Append(redTeamName);
+            stringBuilder.Append(";");
+            stringBuilder.Append(bluePoints);
+            stringBuilder.Append(";");
+            stringBuilder.Append(redPoints);
+            streamWriter.Write(stringBuilder.ToString());
+            streamWriter.Flush();
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream());
+            string s = streamReader.ReadLine();
+            if(s != null)
+                if (s.Equals("SUCCESS"))
+                    return true;
+            return false;
+        }
+    }
+}
