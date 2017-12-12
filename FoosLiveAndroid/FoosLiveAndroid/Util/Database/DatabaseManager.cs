19ab129745bf9f1a08dd4ae1bb1d7afbadffe23c
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-12-12 15:26:59 +0200

    Upgrade DatabaseManager

--- FoosLiveAndroid/FoosLiveAndroid/Util/Database/DatabaseManager.cs
+++ FoosLiveAndroid/FoosLiveAndroid/Util/Database/DatabaseManager.cs
@@ -4,38 +4,46 @@ using System.Collections.Generic;
 using FoosLiveAndroid.Model.Interface;
 using FoosLiveAndroid.Model;
 using System.Threading.Tasks;
-using Android.Util;
 
 namespace FoosLiveAndroid.Util.Database
 {
     public static class DatabaseManager
     {
-
+        // Request properties
         private static readonly string ConnectionUrl = PropertiesManager.GetProperty("connection_url");
         private static readonly string OperationSuccess = PropertiesManager.GetProperty("operation_success");
-        private static readonly int GetTimeout = PropertiesManager.GetIntProperty("get_timeout");
+        private static readonly int Timeout = PropertiesManager.GetIntProperty("db_timeout");
 
+        // Request statements formats
+        private static readonly string InsertGameFormat = PropertiesManager.GetProperty("insert_game_format");
+        private static readonly string InsertGoalFormat = PropertiesManager.GetProperty("insert_goal_format");
+        private static readonly string InsertEventFormat = PropertiesManager.GetProperty("insert_event_format");
+        private static readonly string GetHistoryFormat = PropertiesManager.GetProperty("get_history_format");
+       
         /// <summary>
         /// Inserts a game into the remote database. 
         /// </summary>
         /// <param name="blueTeamName"> Blue team name</param>
         /// <param name="redTeamName">Red team name</param>
-        /// <returns>A task that returns int. The int is the id of the inserted game, or -1 if error happens.
+        /// <returns>The id of the inserted game, or null if error happens.
         /// This id is used to specify which game to add goals and events to.</returns>
         public static async Task<int> InsertGame(string blueTeamName, string redTeamName) {
             var request = (HttpWebRequest)WebRequest.Create(ConnectionUrl);
             request.Method = WebRequestMethods.Http.Post;
-            request.Timeout = GetTimeout;
+            request.Timeout = Timeout;
+
             var streamWriter = new StreamWriter(request.GetRequestStream());
-            streamWriter.Write($"InsertGame;{blueTeamName};{redTeamName}");
+            // Prepare query statement
+            streamWriter.Write(string.Format(InsertGameFormat, blueTeamName, redTeamName));
+
             streamWriter.Flush();
-            var httpWebResponse = (HttpWebResponse)await request.GetResponseAsync();
+            var httpWebResponse = (HttpWebResponse) await request.GetResponseAsync();
+
             var streamReader = new StreamReader(httpWebResponse.GetResponseStream());
             string idUnconverted = await streamReader.ReadToEndAsync();
-            if (int.TryParse(idUnconverted, out int id))
-                return id;
-            else
-                return -1;
+            int.TryParse(idUnconverted, out int id);
+
+            return id;
         }
         /// <summary>
         /// Inserts a goal into the remote database.
@@ -43,18 +51,24 @@ namespace FoosLiveAndroid.Util.Database
         /// <param name="gameId">The game id in the database. It is recommended to use the int returned by InsertGame,
         /// otherwise the method might not work correctly or at all.</param>
         /// <param name="teamName">The team name of the team who scored.</param>
-        /// <returns></returns>
+        /// <returns>Request result</returns>
         public static async Task<bool> InsertGoal(int gameId, string teamName)
         {
+            // Set up request 
             var request = (HttpWebRequest)WebRequest.Create(ConnectionUrl);
             request.Method = WebRequestMethods.Http.Post;
-            request.Timeout = GetTimeout;
+            request.Timeout = Timeout;
+
+            // Set up request statement
             var streamWriter = new StreamWriter(await request.GetRequestStreamAsync());
-            streamWriter.Write($"InsertGoal;{gameId};{teamName}");
+            streamWriter.Write(string.Format(InsertGoalFormat, gameId, teamName));
             streamWriter.Flush();
+
+            // Get response
             var httpWebResponse = (HttpWebResponse)await request.GetResponseAsync();
             var streamReader = new StreamReader(httpWebResponse.GetResponseStream());
             string response = await streamReader.ReadToEndAsync();
+
             return response.Equals(OperationSuccess);
         }
 
@@ -64,18 +78,24 @@ namespace FoosLiveAndroid.Util.Database
         /// <param name="gameId">The game id in the database. It is recommended to use the int returned by InsertGame,
         /// otherwise the method might not work correctly or at all.</param>
         /// <param name="details">The details of the event, in string form.</param>
-        /// <returns></returns>
+        /// <returns>Request result</returns>
         public static async Task<bool> InsertEvent(int gameId, string details)
         {
+            // Set up request 
             var request = (HttpWebRequest)WebRequest.Create(ConnectionUrl);
             request.Method = WebRequestMethods.Http.Post;
-            request.Timeout = GetTimeout;
+            request.Timeout = Timeout;
+
+            // Set up request statement
             var streamWriter = new StreamWriter(request.GetRequestStream());
-            streamWriter.Write($"InsertEvent;{gameId};{details}");
+            streamWriter.Write(string.Format(InsertEventFormat, gameId, details));
             streamWriter.Flush();
+
+            // Get response
             var httpWebResponse = (HttpWebResponse)await request.GetResponseAsync();
             var streamReader = new StreamReader(httpWebResponse.GetResponseStream());
             string response = await streamReader.ReadToEndAsync();
+
             return response.Equals(OperationSuccess);
         }
 
@@ -84,16 +104,22 @@ namespace FoosLiveAndroid.Util.Database
         /// </summary>
         /// <returns>A task that returns a list of IHistory objects.</returns>
         public static async Task<List<IHistory>> GetHistory() {
-            var historyData = new List<IHistory>();
+            // Set up request 
             var request = (HttpWebRequest)WebRequest.Create(ConnectionUrl);
             request.Method = WebRequestMethods.Http.Post;
-            request.Timeout = GetTimeout;
+            request.Timeout = Timeout;
+
+            // Set up request statement
             var streamWriter = new StreamWriter(request.GetRequestStream());
-            streamWriter.Write("GetHistory");
+            streamWriter.Write(GetHistoryFormat);
             streamWriter.Flush();
+
+            // Get response
             var httpWebResponse = (HttpWebResponse) await request.GetResponseAsync();
             var streamReader = new StreamReader(httpWebResponse.GetResponseStream());
+            // Parse response
             string response;
+            var historyData = new List<IHistory>();
             while ((response = streamReader.ReadLine()) != null)
             {
                 historyData.Add(new History(response.Split(';')));
